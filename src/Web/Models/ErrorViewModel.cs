d766aa3679a4394f610deab452915276e02f49c2
Oleg Iaremchuk <martelskiy@gmail.com>
2018-06-27 15:51:44 +0200

    Added new property to error view model (#4)

--- src/Web/Models/ErrorViewModel.cs
+++ src/Web/Models/ErrorViewModel.cs
@@ -7,5 +7,7 @@ namespace Web.Models
         public string RequestId { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+
+        public string ErrorText { get; set; }
     }
 }
