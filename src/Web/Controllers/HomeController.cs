cc33da78c3517302cee7d43c12294747e823d4f1
sebastian.wallmark <sebastian.wallmark@studentum.se>
2018-06-27 14:19:42 +0200

    Changes of stuff

--- src/Web/Controllers/HomeController.cs
+++ src/Web/Controllers/HomeController.cs
@@ -17,7 +17,7 @@ namespace Web.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "foobar";
 
             return View();
         }
