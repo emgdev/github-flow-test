8abade030630ed5a31da8e2adf30888549beab52
SWallmark <sebastian.wallmark@educations.com>
2018-06-27 15:16:33 +0200

    Flowing (#2)
    * re #666 - The ticket
    
    Added some additional information
    
    * re #666 - The ticket
    
    Updated privacy policy
    
    * Small change
    
    * Smaller change
    
    * Ticket 67890 (#3)
    
    * nice background
    
    * for the menu too
    
    * updated background color
    
    * re #666 - The ticket
    
    Updated privacy policy
    
    * Ticket 67890 (#3)
    
    * nice background
    
    * for the menu too
    
    * updated background color
    
    * Small change
    
    * Smaller change
    
    * re #666 - The ticket
    
    Updated privacy policy

--- src/Web/Controllers/HomeController.cs
+++ src/Web/Controllers/HomeController.cs
@@ -24,7 +24,7 @@ namespace Web.Controllers
 
         public IActionResult Contact()
         {
-            ViewData["Message"] = "Your contact page.";
+            ViewData["Message"] = "Your contact page is mine";
 
             return View();
         }
